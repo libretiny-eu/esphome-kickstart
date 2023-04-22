afd09a626fd78831adf80a0cd24e5b5d41e86455
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-04-22 19:35:43 +0200

    Add GitHub Actions workflow

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+mkdir -p build/
+cd yaml/
+configs=("bk7231t" "bk7231n" "rtl8710bn-2mb-468k")
+for config in ${configs[@]}; do
+	esphome compile kickstart-${config}.yaml
+	cp .esphome/build/kickstart-${config}/.pioenvs/kickstart-${config}/firmware.uf2 ../build/kickstart-${config}-$1.uf2
+	cp .esphome/build/kickstart-${config}/.pioenvs/kickstart-${config}/*.ota.rbl ../build/ || true
+	cp .esphome/build/kickstart-${config}/.pioenvs/kickstart-${config}/*.ota.ug.bin ../build/ || true
+done
