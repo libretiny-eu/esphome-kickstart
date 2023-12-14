e4ad803479ec45c4c07548d47537ad2660a48b74
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-12-14 14:31:00 +0100

    Use ESPHome official addon, use local components

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 mkdir -p build/
 cd yaml/
-configs=("bk7231t" "bk7231n" "rtl8710bn-2mb-788k")
+configs=("bk7231t" "bk7231n" "rtl8710bn")
 for config in ${configs[@]}; do
 	esphome compile kickstart-${config}.yaml
 	cp .esphome/build/kickstart-${config}/.pioenvs/kickstart-${config}/firmware.uf2 ../build/kickstart-${config}-$1.uf2
